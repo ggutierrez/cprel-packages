5f6f47c1dcd56ac190e5bb8b8e6f61eb92ddc026
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2011-11-02 17:41:18 +0100

    Added some functions for package processing

--- tools/stats.cpp
+++ tools/stats.cpp
@@ -19,10 +19,13 @@ private:
   MPG::GRelation dependencies_;
   MPG::GRelation conflicts_;
   MPG::GRelation provides_;
+  /// Temporal information
+  int concretes_;
 public:
   ///Constructor
   RelationWriter(void)
-    : packages_(3), dependencies_(2), conflicts_(2), provides_(2) {}
+    : packages_(3), dependencies_(2), conflicts_(2), provides_(2),
+      concretes_(0) {}
   /// Destructor
   virtual ~RelationWriter(void) {}
   /// Callback for a package
@@ -46,6 +49,9 @@ public:
   }
   /// Callback for a dependency
   virtual void provides(unsigned int package, unsigned int provides, const char*) {
+    if (package == provides)
+      concretes_++;
+
     MPG::Tuple t({(int)package,(int)provides});
     provides_.add(t);
   }
@@ -53,21 +59,72 @@ public:
   ProblemDesc problem(void) {
     return ProblemDesc(packages_,dependencies_,conflicts_,provides_);
   }
+  /// Returns the number of read concrete packages
+  int concretes(void) const {
+    return concretes_;
+  }
 };
 
 ProblemDesc process(std::istream& kcudf) {
   std::cout << "Transforming problem into relations" << std::endl; 
   RelationWriter rw;
   read(kcudf,rw);
+  std::cout << "Concrete packages: " << rw.concretes() << std::endl; 
   return rw.problem();
 }
 
-void bddStats(const ProblemDesc& problem) {
+MPG::GRelation oneProvider(const ProblemDesc& problem) {
+  // providers is the 4-th term of the problem description
+  const MPG::GRelation& providers = std::get<3>(problem);
+  MPG::GRelation oneProv(providers);
+  auto u = oneProv.unique(0).shiftRight(1);
+  /*
+  std::cout // << "One provider " << u << std::endl
+	    << "\tnumber: " << u.cardinality() << std::endl
+	    << "\tout of " << providers.cardinality() << std::endl;  
+  */
+  return u;
+}
+MPG::GRelation concretes(const ProblemDesc& problem) {
+  /// A concrete package provides itself
+  const MPG::GRelation& providers = std::get<3>(problem);
+  MPG::GRelation oneProv(providers);
+  auto u = oneProv.unique(0).intersect(providers);
+  // if all the virtuals have at least one provider this is accurate:
+  
+
+  // Get a unary relation with the packages from the description
+  auto packages = std::get<0>(problem).shiftRight(2);
+  //std::cout << "Packages!! " << packages.cardinality() << std::endl;
+  //auto packages_packages = packages.times(packages);
+  
+  // intersect both relations to get the concretes
+  return packages.intersect(u.project(1).complement());
+}
+
+MPG::GRelation filterPackages(const ProblemDesc& problem, int keep, int install) {
+  MPG::GRelation tmp(2);
+  tmp.add(MPG::Tuple({keep,install}));
+  
+  auto allPossible = tmp.timesULeft(1);
+  auto filtered = allPossible.intersect(std::get<0>(problem));
+  std::cout << "Packages matching " << keep << " " << install 
+	    << " " << filtered.cardinality() << std::endl; 
+  return filtered;
+}
+
+void problemStats(const ProblemDesc& problem) {
   std::cout << "Problem statistics: " << std::endl
 	    << "\tPackages: " << std::get<0>(problem).cardinality() << std::endl
 	    << "\tDependencies: " << std::get<1>(problem).cardinality() << std::endl
 	    << "\tconflicts: " << std::get<2>(problem).cardinality() / 2 << std::endl
 	    << "\tProvides: " << std::get<3>(problem).cardinality() << std::endl;
+  //oneProvider(problem);
+  //filterPackages(problem,0,1);
+  std::cout << "Concrete packages: " << concretes(problem).cardinality() << std::endl; 
+}
+
+void bddStats(const ProblemDesc& problem) {
   
   auto& factory = MPG::VarImpl::factory();
  
@@ -75,7 +132,7 @@ void bddStats(const ProblemDesc& problem) {
 	    << "\tNodes used: " << factory.ReadNodeCount()
 	    << "\tReordering time: " << factory.ReadReorderingTime() << std::endl;
 
-  std::cout << "Will start reordering: " << std::endl; 
+  //std::cout << "Will start reordering: " << std::endl; 
   //factory.ReduceHeap(CUDD_REORDER_ANNEALING,1000);
 }
 
@@ -90,7 +147,7 @@ int main(int argc, char* argv[]) {
     }
     auto problem = process(is);
     bddStats(problem);
-    
+    problemStats(problem);
   } else {
     std::cerr << "Called with wrong number of arguments" << std::endl; 
     return 1;
