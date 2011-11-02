42b96b33accee0706c7fcef8d5b90ac628310234
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2011-11-02 20:50:43 +0100

    Relation reader moved to separate file

--- tools/stats.cpp
+++ tools/stats.cpp
@@ -1,79 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <tuple>
-#include <kcudf/kcudf.hh>
-#include <rel/grelation.hh>
+#include <boost/chrono.hpp>
+#include <cmath>
+#include <solversupport/reader.hh>
 #include <bdddomain/manager.hh>
 
-/// A problem is a tuple of ground relations: <Packages,Dependencies,Conflicts,Provides>
-typedef std::tuple<MPG::GRelation,MPG::GRelation,MPG::GRelation,MPG::GRelation,MPG::GRelation>
-ProblemDesc;
+namespace bc = boost::chrono;
 
-/**
- * \brief Stores the information of a kcudf file into a relation.
- */
-class RelationWriter : public KCudfWriter {
-private:
-  /// The relations when the contents are read
-  MPG::GRelation packages_;
-  MPG::GRelation dependencies_;
-  MPG::GRelation conflicts_;
-  MPG::GRelation provides_;
-  MPG::GRelation concretes_;
-public:
-  ///Constructor
-  RelationWriter(void)
-    : packages_(3), dependencies_(2), conflicts_(2), provides_(2),
-      concretes_(1) {}
-  /// Destructor
-  virtual ~RelationWriter(void) {}
-  /// Callback for a package
-  virtual void package(unsigned int id, bool keep, bool install, const char*) {
-    int k = keep ? 1 : 0;
-    int i = install ? 1 : 0;
-    MPG::Tuple t({id,k,i});
-    packages_.add(t);
-  }
-  /// Callback for a dependency
-  virtual void dependency(unsigned int package, unsigned int depends, const char*) {
-    MPG::Tuple t({(int)package,(int)depends});
-    dependencies_.add(t);
-  }
-  /// Callback for a dependency
-  virtual void conflict(unsigned int package, unsigned int conflict, const char*) {
-    MPG::Tuple t({(int)package, (int)conflict});
-    MPG::Tuple t2({(int)conflict, (int)package});
-    conflicts_.add(t);
-    conflicts_.add(t2);
-  }
-  /// Callback for a dependency
-  virtual void provides(unsigned int package, unsigned int provides, const char*) {
-    if (package == provides) {
-      MPG::Tuple c({(int)package});
-      concretes_.add(c);
-    }
 
-    MPG::Tuple t({(int)package,(int)provides});
-    provides_.add(t);
-  }
-  /// Returns a tuple with the relations
-  ProblemDesc problem(void) {
-    return ProblemDesc(packages_,dependencies_,conflicts_,provides_,concretes_);
-  }
-  /// Returns the number of read concrete packages
-  int concretes(void) const {
-    return concretes_.cardinality();
-  }
-};
 
-ProblemDesc process(std::istream& kcudf) {
+CPRelPkg::ProblemDesc process(std::istream& kcudf) {
   std::cout << "Transforming problem into relations" << std::endl; 
-  RelationWriter rw;
+  CPRelPkg::RelationWriter rw;
   read(kcudf,rw);
   return rw.problem();
 }
 
-MPG::GRelation oneProvider(const ProblemDesc& problem) {
+MPG::GRelation oneProvider(const CPRelPkg::ProblemDesc& problem) {
   // providers is the 4-th term of the problem description
   const MPG::GRelation& providers = std::get<3>(problem);
   MPG::GRelation oneProv(providers);
@@ -86,11 +30,11 @@ MPG::GRelation oneProvider(const ProblemDesc& problem) {
   return u;
 }
 
-MPG::GRelation concretes(const ProblemDesc& problem) {
+MPG::GRelation concretes(const CPRelPkg::ProblemDesc& problem) {
   return std::get<4>(problem);
 }
 
-MPG::GRelation filterPackages(const ProblemDesc& problem, int keep, int install) {
+MPG::GRelation filterPackages(const CPRelPkg::ProblemDesc& problem, int keep, int install) {
   MPG::GRelation tmp(2);
   tmp.add(MPG::Tuple({keep,install}));
   
@@ -100,7 +44,7 @@ MPG::GRelation filterPackages(const ProblemDesc& problem, int keep, int install)
 }
 
 
-void problemStats(const ProblemDesc& problem) {
+void problemStats(const CPRelPkg::ProblemDesc& problem) {
   std::cout << "Problem statistics: " << std::endl
 	    << "\tPackages: " << std::get<0>(problem).cardinality() << std::endl
 	    << "\tDependencies: " << std::get<1>(problem).cardinality() << std::endl
@@ -108,10 +52,10 @@ void problemStats(const ProblemDesc& problem) {
 	    << "\tProvides: " << std::get<3>(problem).cardinality() << std::endl;
   //oneProvider(problem);
   //filterPackages(problem,0,1);
-  std::cout << "Concrete packages: " << concretes(problem).cardinality() << std::endl; 
+  //std::cout << "Concrete packages: " << concretes(problem).cardinality() << std::endl; 
 }
 
-void bddStats(const ProblemDesc& problem) {
+void bddStats(const CPRelPkg::ProblemDesc& problem) {
   
   auto& factory = MPG::VarImpl::factory();
  
@@ -132,7 +76,11 @@ int main(int argc, char* argv[]) {
       std::cout << "Error opening the file: " << kcudf << std::endl; 
       return 1;
     }
+    bc::system_clock::time_point start = bc::system_clock::now();
     auto problem = process(is);
+    bc::duration<double> sec = bc::system_clock::now() - start;
+    std::cout << "Reading time " << sec.count() << " seconds\n";
+   
     bddStats(problem);
     problemStats(problem);
   } else {
