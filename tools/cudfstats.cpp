20d7dc78ca7d8ec8f80a2f1f8c397b2129ef0667
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2011-11-14 10:33:49 +0100

    Added example that calls the unsaparser

---
+++ tools/cudfstats.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cudf/cudf.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cerr << "Error, no input provided" << endl;
+    return 1;
+  }
+  // the input file is the first argument
+  FILE *input_file = fopen(argv[1], "r");
+  if (input_file == NULL) {
+    cerr << "Error opening file: " << argv[1] << endl;
+    return 1;
+  }
+  // parse the file, using unsa parser
+  switch(parse_cudf(input_file)) {
+  case 0: break;
+  case 1: break;
+  case 2: break;
+  }
+  fclose(input_file);
+  cout << "CUDF statistics" << endl;
+  return 0;
+}
